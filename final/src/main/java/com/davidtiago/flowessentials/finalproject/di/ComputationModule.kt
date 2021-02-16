8ecd4642fd4128cd727b4158d021d2e5f86a9d45
David Tiago Conceição <davidtc.developer@gmail.com>
2021-02-16 17:17:13 -0300

    Allows injection by making singleton

--- final/src/main/java/com/davidtiago/flowessentials/finalproject/di/ComputationModule.kt
+++ final/src/main/java/com/davidtiago/flowessentials/finalproject/di/ComputationModule.kt
@@ -7,6 +7,7 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.components.SingletonComponent
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.asCoroutineDispatcher
@@ -22,7 +23,7 @@ annotation class CacheDispatcher
 annotation class ComputingDispatcher
 
 @Module
-@InstallIn(ActivityComponent::class)
+@InstallIn(SingletonComponent::class)
 abstract class ComputationModule {
 
     @Binds
